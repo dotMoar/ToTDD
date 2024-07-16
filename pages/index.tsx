bc59c0e1f241205529d174649bb8f4e5fa7d851d
Seva <sebastian.diaz@outlook.com>
2024-07-15 22:32:39 -0400

    chore(init): init proyect with basic config to next, typescript and tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div className="min-h-screen bg-zinc-700 text-white flex items-center">
+      <div className="grid grid-cols-3 gap-4 w-full h-screen">
+        <div className="columns">Columna Uno</div>
+        <div className="columns">Columna Dos</div>
+        <div className="columns">Columna Tres</div>
+      </div>
+    </div>
+  )
+}
